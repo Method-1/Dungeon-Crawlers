ce23e3b7283443816450279e3b616f9d970f6d25
Adi Abdulahovic <70193827+Method-1@users.noreply.github.com>
2020-12-07 19:21:31 +0100

    Modified characters.py with username attribute

--- characters.py
+++ characters.py
@@ -1,26 +1,30 @@
 class Characters:
-    def __init__(self, character, initiative, endurance, attack, agility, special):
+    def __init__(self, name, character, initiative, endurance, attack, agility, special):
+        self.name = name
         self.character = character
         self.initiative = initiative
         self.endurance = endurance
         self.attack = attack
         self.agility = agility
         self.special = special
-        self.allstats = {'Class': f'{character}', 'Initiative': f'{initiative}', 'Endurance': f'{endurance}',
+        self.allstats = {'Username': f'{name}', 'Class': f'{character}', 'Initiative': f'{initiative}', 'Endurance': f'{endurance}',
             'Attack': f'{attack}', 'Agility': f'{agility}', 'Special': f'{special}'}
 
 class Knight(Characters):
-    def __init__(self):
-        super().__init__('Knight', 5, 9, 6, 4, "Shield Block")
+    def __init__(self, name):
+        self.name = name
+        super().__init__(name, 'Knight', 5, 9, 6, 4, "Shield Block")
 
 class Wizard(Characters):
-    def __init__(self):
-        super().__init__('Wizard', 6, 4, 9, 5, "Blinding Light")
+    def __init__(self, name):
+        self.name = name
+        super().__init__(name, 'Wizard', 6, 4, 9, 5, "Blinding Light")
 
 class Thief(Characters):
-    def __init__(self):
-        super().__init__('Thief', 7, 5, 5, 7, 'Critical Hit')
+    def __init__(self, name):
+        self.name = name
+        super().__init__(name, 'Thief', 7, 5, 5, 7, 'Critical Hit')
 
-
-test_character = Knight()
+name = input("Username: ")
+test_character = Knight(name)
 print(test_character.allstats)
