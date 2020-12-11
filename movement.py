71ce65ff1110ec14881a37bf00fff7a66219e443
Lenjuz <lennart864@hotmail.com>
2020-12-11 18:43:01 +0100

    nyaste kartan+move

--- movement.py
+++ movement.py
@@ -1,93 +1,108 @@
-from DungeonMap import DungeonMap
+from DungeonWorld import DungeonMap
 
-b = int(input("How big do you want your map? "))
+# MENY SAKER
+mapsize = int(input("How big do you want your map? "))
+corner = int(input("Which corner do you want to start in?\nTop Left = 1\nBottom Left = 2\nTop Right = 3\nBottom Left = 4\n"))
+if corner == 1:
+    startzone = (0,0)
+    endzone = (mapsize - 1, mapsize - 1)
+
+elif corner == 2:
+    startzone = (0, mapsize - 1)
+    endzone = (mapsize - 1, 0)
+
+elif corner == 3:
+    startzone = (mapsize - 1, 0)
+    endzone = (0, mapsize - 1)
+
+elif corner == 4:
+    startzone = (mapsize - 1, mapsize - 1)
+    endzone = (0,0)
 
 class Movement:
     def __init__(self):
-        self.start = (0,0)
-        self.goal = (b,b)
-        self.player = (0,0)
-        self.walls = ((b + 1), (b + 1))
-
-
+        self.start = (startzone[0],startzone[1])
+        self.goal = endzone
+        self.player = startzone
+        self.walls = (0,0)
+                
     def move_player(self, d):
         x = self.player[0]
         y = self.player[1]
-        pos = None
+        pos = startzone
 
         if d == 'r':
             pos = (x + 1, y)
+            # REMOVE ME DEV TEXT
             print(pos)
             print("pos")
             print(self.player)
             print("player")
+            print(self.goal)
 
-
-        if d == 'l':
+        elif d == 'l':
             pos = (x - 1, y)
+            # REMOVE ME DEV TEXT
             print(pos)
             print("pos")
             print(self.player)
             print("player")
 
-        if d == 'u':
+        elif d == 'u':
             pos = (x, y - 1)
+            # REMOVE ME DEV TEXT
             print(pos)
             print("pos")
             print(self.player)
             print("player")
 
-        if d == 'd':
+        elif d == 'd':
             pos = (x, y + 1)
+            # REMOVE ME DEV TEXT
             print(pos)
             print("pos")
             print(self.player)
             print("player")
 
+        else:
+            pass
+ 
         if pos not in self.walls:
             self.player = pos
+
 
         if pos == self.goal:
             print("You made it to the end!")
 
-        if (pos[1] > b):
-            print("Outside bounds Y")
+        if (pos[1] < 0) or (pos[0] < 0) or (pos[0] > (mapsize - 1)) or (pos[1] > (mapsize - 1)):
+            print("Outside bounds!")
             self.player = (x, y)
             pos = self.player
-            print(pos)
-            print(self.player)
-        
-        if (pos[0] > b):
-            print("Outside bounds X")
-            self.player = (x, y)
-            pos = self.player
-            print(pos)
-            print(self.player)
+        else:
+            a.board[self.goal[1]][self.goal[0]] = "E"
+            a.board[self.player[1]][self.player[0]] = "O"
+            a.board[y][x] = "-"
+            a.board[self.start[1]][self.start[0]] = "S"
 
-        if (pos[0] < 0):
-            print("Outside bounds X")
-            self.player = (x, y)
-            pos = self.player
-            print(pos)
-            print(self.player)
 
-        if (pos[1] < 0):
-            print("Outside bounds X")
-            self.player = (x, y)
-            pos = self.player
-            print(pos)
-            print(self.player)
+            
+
+'''
+    def placemonster(self):
+        for i in range(b + 2):
+            randcordx = randint(1, b - 2)
+            randcordy = randint(1, b - 2)
+            print(f'{randcordx},{randcordy}')
+            a.board[randcordx][randcordy] = "O"
+        a.display_map()
+'''
+
+
 
 c = Movement()
-a = DungeonMap(b)
+a = DungeonMap(mapsize)
 
 while c.player != c.goal:
-    d = input("Which way? (r, l, u, d\n")
-    c.move_player(d)
     a.display_map()
-
-
-
-
-
-
+    d = input("Which way? (r, l, u, d)\n")
+    c.move_player(d)
